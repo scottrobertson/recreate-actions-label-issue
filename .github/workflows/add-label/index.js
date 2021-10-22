e6626368010319b15649bcb6a4df4904bbc26d1c
Scott Robertson <scottrobertson@users.noreply.github.com>
2021-10-22 22:36:22 +0000

    Lets try this

---
+++ .github/workflows/add-label/index.js
@@ -0,0 +1,14 @@
+const github = require("@actions/github");
+
+async function run() {
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+  await octokit.rest.issues.addLabels({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.payload.pull_request.number,
+    labels: ["testing"],
+  });
+}
+
+run();
